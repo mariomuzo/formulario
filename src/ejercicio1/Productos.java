1eaba36ec30701f7f0da22ddd60fc1befef3893e
pc <pc@172.16.14.102>
2016-05-27 09:59:05 -0600

    1°modificacion

--- src/ejercicio1/Productos.java
+++ src/ejercicio1/Productos.java
@@ -7,6 +7,52 @@
 package ejercicio1;
 
 public class Productos {
-   //HOLA 
+ public String nombre;
+ public String marca;
+ public Double precio;
+ public int existencia;
+
+    public Productos() {
+    }
+
+    public Productos(String nombre, String marca, Double precio, int existencia) {
+        this.nombre = nombre;
+        this.marca = marca;
+        this.precio = precio;
+        this.existencia = existencia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public int getExistencia() {
+        return existencia;
+    }
+
+    public void setExistencia(int existencia) {
+        this.existencia = existencia;
+    }
+ 
     
 }
